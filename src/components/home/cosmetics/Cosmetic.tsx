02d52a7f4281d9312c52d78742dbf89f3d7a8f82
immortalprogrammist <immortalvelial@icloud.com>
2023-07-17 15:32:12 +0200

    implemented latest cosmetics on main page

---
+++ src/components/home/cosmetics/Cosmetic.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./Cosmetic.module.css";
+
+interface CosmeticProps {
+  id: string;
+  name: string;
+  icon: string;
+  rarity: string;
+}
+
+const Cosmetic: FC<CosmeticProps> = ({ id, name, icon, rarity }) => {
+  return (
+    <div className={`${styles.card} ${styles[`${rarity}`]}`}>
+      <Link to={id}>
+        <img src={icon} alt={name} />
+        <div className={styles.name}>
+          <span>{name}</span>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Cosmetic;
--- src/components/home/cosmetics/LatestCosmetics.tsx
+++ src/components/home/cosmetics/LatestCosmetics.tsx
@@ -1,20 +1,63 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import styles from "./LatestCosmetics.module.css";
+import Cosmetic from "./Cosmetic";
+
+interface Item {
+  id: string;
+  name: string;
+  description: string;
+  images: {
+    icon: string;
+    smallIcon: string;
+  };
+  type: {
+    displayValue: string;
+    value: string;
+  };
+  rarity: {
+    value: string;
+    displayValue: string;
+  };
+  added: string;
+}
+
+const defaultCosmetics: Item[] = [];
 
 const LatestCosmetics = () => {
+  const [cosmetics, setCosmetics] = useState<Item[]>(defaultCosmetics);
+
+  useEffect(() => {
+    axios
+      .get("https://fortnite-api.com/v2/cosmetics/br/new")
+      .then((response) => {
+        setCosmetics(response.data.data.items);
+      });
+  }, []);
+
+  console.log(cosmetics);
+
   return (
     <div className={styles.content}>
-      <div className={styles.card}>Test</div>
-      <div className={styles.card}>Test</div>
-      <div className={styles.card}>Test</div>
-      <div className={styles.card}>Test</div>
-      <div className={styles.card}>Test</div>
-      <div className={styles.card}>Test</div>
-      <div className={styles.card}>Test</div>
-      <div className={styles.card}>Test</div>
-      <div className={styles.card}>Test</div>
-      <div className={styles.card}>Test</div>
-      <div className={styles.card}>Test</div>
-      <div className={styles.card}>Test</div>
+      {cosmetics
+        // .sort((a, b) => {
+        //   return (
+        //     new Date(b.added).setHours(0, 0, 0, 0) -
+        //     new Date(a.added).setHours(0, 0, 0, 0)
+        //   );
+        // })
+        .filter((_, index) => index < 12)
+        .map((cosmetic) => {
+          return (
+            <Cosmetic
+              key={cosmetic.id}
+              id={cosmetic.id}
+              name={cosmetic.name}
+              icon={cosmetic.images.icon}
+              rarity={cosmetic.rarity.value}
+            />
+          );
+        })}
     </div>
   );
 };
